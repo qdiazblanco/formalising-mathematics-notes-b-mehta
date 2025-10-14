414873c55726c2ce683a784d6139926aa1460dde
Enrique Diaz Blanco <qdiazblanco@gmail.com>
2025-10-14 11:48:12 +0000

    hoja 5 de logica terminada

--- FormalisingMathematics2026/Section01logic/Sheet5.lean
+++ FormalisingMathematics2026/Section01logic/Sheet5.lean
@@ -25,19 +25,6 @@ and also the following two new tactics:
 variable (P Q R S : Prop)
 
 example : P ↔ P := by
-<<<<<<< HEAD:FormalisingMathematics2026/Section01logic/Sheet5.lean
-  sorry
-
-example : (P ↔ Q) → (Q ↔ P) := by
-  sorry
-
-example : (P ↔ Q) ↔ (Q ↔ P) := by
-  sorry
-
-example : (P ↔ Q) → (Q ↔ R) → (P ↔ R) := by
-  sorry
-  -- The pattern `rw` then `assumption` is common enough that it can be abbreviated to `rwa`
-=======
   rfl
 
 example : (P ↔ Q) → (Q ↔ P) := by
@@ -55,22 +42,93 @@ example : (P ↔ Q) → (Q ↔ R) → (P ↔ R) := by
   intro hPQ hQR
   rw [hQR] at hPQ
   exact hPQ
->>>>>>> 3b9c308 (media hoja 5, me ha petao el Lean):FormalisingMathematics2025/Section01logic/Sheet5.lean
 
 example : P ∧ Q ↔ Q ∧ P := by
-  sorry
+  constructor
+  · intro hPyQ
+    cases' hPyQ with hP hQ
+    constructor
+    · exact hQ
+    · exact hP
+  · intro hQyP
+    cases' hQyP with hQ hP
+    constructor
+    · exact hP
+    · exact hQ
+-- or
+example : P ∧ Q ↔ Q ∧ P := by
+  constructor <;> --uses the next tactics for every goal
+  · intro h
+    cases' h with h1 h2
+    exact ⟨h2,h1⟩ /-means the proof is the pair (h2,h1)
+    it works for a '∧' statement, i don't know about '∨' yet -/
 
 example : (P ∧ Q) ∧ R ↔ P ∧ Q ∧ R := by
-  sorry
+  constructor
+  · intro h
+    cases' h with hPyQ hR
+    cases' hPyQ with hP hQ
+    exact ⟨hP,hQ,hR⟩
+  · intro hPQR
+    cases' hPQR with hP hQyR
+    cases' hQyR with hQ hR
+    exact ⟨⟨hP,hQ⟩, hR⟩
+    /- instead of 'exact' we could use
+    constructor
+    · exact ⟨hP,hQ⟩
+    · exact hR
+    -/
+
 
 example : P ↔ P ∧ True := by
-  sorry
+  constructor
+  · intro hP
+    exact ⟨hP, trivial⟩
+  · rintro ⟨hP, hT⟩
+    exact hP
 
 example : False ↔ P ∧ False := by
-  sorry
+  constructor
+  · intro hF
+    exfalso
+    exact hF
+  · rintro ⟨hP,hF⟩ /-I don't know why rintro here neither applies assumption afterwards
+                    nor let's me write 'rintro h ⟨hP,hF⟩' as before -/
+    exact hF
+    /-
+    intro h
+    cases' h with hP hF
+    exact hF
+-/
 
 example : (P ↔ Q) → (R ↔ S) → (P ∧ R ↔ Q ∧ S) := by
-  sorry
+  rintro ⟨hPQ, hQP⟩ ⟨hRS,hSR⟩
+  constructor
+  · rintro ⟨hP, hR⟩
+    constructor
+    · apply hPQ
+      exact hP
+    · apply hRS
+      exact hR
+--or easier using 'rw' twice, since 'rw' uses 'rfl' afterwards
+example : (P ↔ Q) → (R ↔ S) → (P ∧ R ↔ Q ∧ S) := by
+  intro h1 h2
+  rw [h1]
+  rw [h2]
 
 example : ¬(P ↔ ¬P) := by
-  sorry
+  intro h
+  cases' h with h1 h2
+  by_cases hP : P
+  · change P → (P → False) at h1
+    apply h1
+    exact hP
+    exact hP
+  · change P → False at hP
+    apply hP
+    apply h2
+    change ¬P at hP
+    apply hP
+/- In his solution he uses 'have' which I guess it is like stating a lemma
+and proving it inside the big proof, but I didn't want to use that.
+Maybe what I came up with is unelegant or something, but it returns 'no goals'-/
